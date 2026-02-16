dcfdc051269946f77ec98685154eafd352ddb35f
Aleksey40000 <alekseyfilippov9392@gmail.com>
2026-02-16 18:03:51 +0700

    Доработки

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
-from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.contrib.auth import login
+from django.shortcuts import redirect
 
 from .forms import RegisterForm
 
